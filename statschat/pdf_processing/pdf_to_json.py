82fa6250fdeae5d71abdbeabb9c5faa403f2c21c
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2025-01-29 11:55:53 +0000

    updated script to download all pdfs from KNBS website

--- statschat/pdf_processing/pdf_to_json.py
+++ statschat/pdf_processing/pdf_to_json.py
@@ -1,163 +1,95 @@
 # %%
-# import modules
-import PyPDF2
-import json
+import requests
+from bs4 import BeautifulSoup
 from pathlib import Path
-import datetime
-import numpy as np
+from urllib.parse import urlparse
 
 # %%
-# set relative paths
-
+# Set relative paths
 DATA_DIR = Path.cwd().joinpath("data/pdf_downloads")
-JSON_DIR = Path.cwd().joinpath("data/json_conversions")
 
-
-def get_name_and_meta(file_path):
-    file_name = pdf_file_path.name
-    pdf_metadata = PyPDF2.PdfReader(pdf_file_path)
-    pdf_metadata = pdf_metadata.metadata
-
-    return (file_name, pdf_metadata)
-
-
-def get_date(metadata, name, counter):
-    # pdf date, time and year (may need to add for modification date)
-    try:
-        pdf_creation_date = str(metadata.creation_date)
-        print(f"no issue with metadata for {name}")
-
-    except Exception as e:
-        counter += 1
-        pdf_creation_date = datetime.datetime.now().strftime("%Y-%m-%d")
-        print(f"An error occurred for file {name}: {e}")
-        print(f"Total number of files with errors: {count}")
-
-    return pdf_creation_date, counter
-
-
-def determine_dates(pdf_creation_date, pdf_modification_date):
-    pdf_creation_year = pdf_creation_date[:4]
-    pdf_creation_month = pdf_creation_date[5:7]
-
-    pdf_modification_month = pdf_modification_date[5:7]
-    pdf_modification_year = pdf_modification_date[:4]
-
-    # in case creation date empty
-    if pdf_creation_year == "None":
-        pdf_creation_year = pdf_modification_year
-    if pdf_creation_month == "":
-        pdf_creation_month = pdf_modification_month
-
-    # if months below for pdf creation and modification are different
-    # An error will come in the pdf hyperlink
-    if int(pdf_creation_month) < int(pdf_modification_month) and int(
-        pdf_creation_year
-    ) == int(pdf_modification_year):
-        pdf_month = pdf_modification_month
-        pdf_year = pdf_modification_year
-    # if years below for pdf creation year and modification year are different
-    # An error will come in the pdf hyperlink
-    elif int(pdf_creation_year) < int(pdf_modification_year) and int(
-        pdf_creation_month
-    ) > int(pdf_modification_month):
-        pdf_year = pdf_modification_year
-        pdf_month = pdf_modification_month
-    else:
-        pdf_year = pdf_modification_year
-        pdf_month = pdf_modification_month
-
-    return pdf_year, pdf_month
-
-
-def build_json(
-    pdf_year, pdf_month, pdf_creation_date, pdf_file_path, pdf_metadata, file_name
-):
-    # create new dict
-    pdf_info = {}
-    pdf_info["id"] = str(np.random.randint(1000000, 9999999))
-    pdf_info["latest"] = True
-    pdf_info["url"] = (
-        f"https://www.knbs.or.ke/wp-content/uploads/{pdf_year}/{pdf_month}/" + file_name
-    )
-    pdf_info["release_date"] = pdf_creation_date[:10]  # date only
-    pdf_info["release_type"] = "pdf_publication"
-    pdf_info["url_keywords"] = ["test", "bulletin"]
-    pdf_info["contact_name"] = "Joe Bloggs"
-    pdf_info["contact_link"] = "mailto:test@knbs.com"
-
-    try:
-        pdf_info["title"] = str(pdf_metadata.title)
-    except Exception as e:
-        title_from_filename = file_name.replace(".pdf", "")
-        pdf_info["title"] = title_from_filename
-        print(f"An error occurred for file {file_name}: {e}")
-
-    # create list to store
-    pages_text = []
-
-    with open(pdf_file_path, "rb") as pdf_file:
-        # read file
-        pdf_reader = PyPDF2.PdfReader(pdf_file)
-
-        # read every page
-        for page_num in range(len(pdf_reader.pages)):
-            # read page wise data from pdf file
-            page = pdf_reader.pages[page_num]
-
-            # extract data from page to text
-            text = page.extract_text().split("\n")
-            # convert list of strings into one large string
-            text = "".join(text)
-
-            # get page link
-            page_link = pdf_info["url"]
-
-            # add text of page to array
-            pages_text.append(
-                {
-                    "page_number": page_num + 1,
-                    "page_url": page_link + "#page=" + str(page_num + 1),
-                    "page_text": text,
-                }
-            )
-
-        # create nested dictionary
-        pdf_info["content"] = pages_text
-
-        # Check if JSON_DIR exists, if not, create the folder
-        if not JSON_DIR.exists():
-            JSON_DIR.mkdir(parents=True, exist_ok=True)
-
-        with open(JSON_DIR / f"{pdf_file_path.stem}.json", "w") as json_file:
-            json.dump(pdf_info, json_file, indent=4)
-
-        # print JSON
-        # print(json.dumps([pdf_info], indent=4))
-
+# Check if DATA_DIR exists, if not, create the folder
+if not DATA_DIR.exists():
+    DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # %%
-# create counter
-count = 0
+# get all webpages on KNBS website that have PDFs and add them to a list
 
-pdf_list = DATA_DIR.glob("*.pdf")
+all_pdf_links = []
 
-# loop through folder to get filepaths
-for pdf_file_path in DATA_DIR.glob("*.pdf"):
-    file_name, pdf_metadata = get_name_and_meta(pdf_file_path)
+page = 1
+base_url = f'https://www.knbs.or.ke/all-reports/page'
 
-    pdf_creation_date, count = get_date(pdf_metadata, file_name, count)
+continue_search = True
 
-    try:
-        pdf_modification_date = str(pdf_metadata.modification_date)
-    except AttributeError:
-        print(f"An error fetching the modification date occurred for file {file_name}")
-        pdf_modification_date = pdf_creation_date
+while continue_search:
+    url = base_url + str(page) + '/'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-    pdf_year, pdf_month = determine_dates(pdf_creation_date, pdf_modification_date)
+    # Break the loop if no more quotes are found
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+    
+    if len(pdf_links) == 0:
+        print('I HAVE GOT WEBPAGES')
+        continue_search = False
+    
+    page += 1
+    all_pdf_links.append(url)
 
-    build_json(
-        pdf_year, pdf_month, pdf_creation_date, pdf_file_path, pdf_metadata, file_name
-    )
+print('I HAVE FINISHED')
 
-print(f"Total number of files with errors: {count}")
+# %%
+# print counter
+
+# %%
+# removes last append to list as that webpage has no pdfs (USE THIS)
+
+all_pdf_links = all_pdf_links[:-1]
+all_pdf_links
+
+# %%
+# gets links for every PDF from every KNBS webpage and adds to a list
+
+all_knbs_pdf_file_links = []
+
+for pdf_file in all_pdf_links:
+    
+    url = pdf_file
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+    
+    all_knbs_pdf_file_links.append(pdf_links)
+
+# %%
+# gets page range for PDFs to loop through multiple lists made
+
+pdf_page_range = len(all_knbs_pdf_file_links)
+
+# %%
+# downloads PDFs to relevant folder
+
+for i in range(pdf_page_range):
+    for pdf in all_knbs_pdf_file_links[i]:
+        url = pdf
+        parsed_url = urlparse(url)
+        pdf_name = parsed_url.path
+        actual_pdf_file_name = pdf_name[28:]
+
+        response = requests.get(url)
+        file_path = f"{DATA_DIR}/{actual_pdf_file_name}"
+
+        if response.status_code == 200:
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            print(f"File {actual_pdf_file_name} downloaded successfully")
+        else:
+            print(f"Failed to download file {actual_pdf_file_name}")
+
+
