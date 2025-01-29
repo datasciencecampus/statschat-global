ad0db0639d1418b1a49b3c27456d11c25bde7aa1
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2025-01-29 12:06:17 +0000

    updated pdf_downloader for getting all pdfs, accidentally renamed as pdf to json but corrected

--- statschat/pdf_processing/pdf_downloader.py
+++ statschat/pdf_processing/pdf_downloader.py
@@ -1,5 +1,3 @@
-# %% [markdown]
-# Download all pdfs from certain webpage
 # %%
 import requests
 from bs4 import BeautifulSoup
@@ -15,38 +13,83 @@ if not DATA_DIR.exists():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # %%
-url = "https://www.knbs.or.ke/all-reports/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+# get all webpages on KNBS website that have PDFs and add them to a list
 
-# %%
-# finds pdf refs on webpage
-pdf_links = [
-    a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
-]
+all_pdf_links = []
 
-# %%
-# on separate lines
-webpage_list_pdf_links = "\n".join(pdf_links)
-print(webpage_list_pdf_links)
+page = 1
+base_url = f'https://www.knbs.or.ke/all-reports/page'
 
-# %%
-# number of pdf's
-len(pdf_links)
+continue_search = True
 
-# %%
-for pdf in pdf_links:
-    url = pdf
-    parsed_url = urlparse(url)
-    pdf_name = parsed_url.path
-    actual_pdf_file_name = pdf_name[28:]
-
+while continue_search:
+    url = base_url + str(page) + '/'
     response = requests.get(url)
-    file_path = f"{DATA_DIR}/{actual_pdf_file_name}"
+    soup = BeautifulSoup(response.content, 'html.parser')
 
-    if response.status_code == 200:
-        with open(file_path, "wb") as file:
-            file.write(response.content)
-        print(f"File {actual_pdf_file_name} downloaded successfully")
-    else:
-        print(f"Failed to download file {actual_pdf_file_name}")
+    # Break the loop if no more quotes are found
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+    
+    if len(pdf_links) == 0:
+        print('I HAVE GOT WEBPAGES')
+        continue_search = False
+    
+    page += 1
+    all_pdf_links.append(url)
+
+print('I HAVE FINISHED')
+
+# %%
+# print counter
+
+# %%
+# removes last append to list as that webpage has no pdfs (USE THIS)
+
+all_pdf_links = all_pdf_links[:-1]
+all_pdf_links
+
+# %%
+# gets links for every PDF from every KNBS webpage and adds to a list
+
+all_knbs_pdf_file_links = []
+
+for pdf_file in all_pdf_links:
+    
+    url = pdf_file
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+    
+    all_knbs_pdf_file_links.append(pdf_links)
+
+# %%
+# gets page range for PDFs to loop through multiple lists made
+
+pdf_page_range = len(all_knbs_pdf_file_links)
+
+# %%
+# downloads PDFs to relevant folder
+
+for i in range(pdf_page_range):
+    for pdf in all_knbs_pdf_file_links[i]:
+        url = pdf
+        parsed_url = urlparse(url)
+        pdf_name = parsed_url.path
+        actual_pdf_file_name = pdf_name[28:]
+
+        response = requests.get(url)
+        file_path = f"{DATA_DIR}/{actual_pdf_file_name}"
+
+        if response.status_code == 200:
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            print(f"File {actual_pdf_file_name} downloaded successfully")
+        else:
+            print(f"Failed to download file {actual_pdf_file_name}")
+
+
