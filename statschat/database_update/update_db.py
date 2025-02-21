61706b9574aa72ec1a24b59975edc7183aa82ee1
josh-ons2022 <118263780+josh-ons2022@users.noreply.github.com>
2025-02-21 09:09:22 +0000

    initial detection of pdfs that haven't been converted to jsons

---
+++ statschat/database_update/update_db.py
@@ -0,0 +1,47 @@
+# %%
+# import modules
+import PyPDF2
+import json
+from pathlib import Path
+import datetime
+import numpy as np
+
+# %%
+# set relative paths
+
+DATA_DIR = Path.cwd().joinpath("data/pdf_downloads")
+JSON_DIR = Path.cwd().joinpath("data/json_conversions")
+
+# %%
+json_list = []
+
+for json_file in JSON_DIR.glob("*.json"):
+    # remove .json from end
+    #print(json_file.name[:-5])
+    
+    # append to list
+    json_list.append(json_file.name[:-5])
+
+#json_list = json_list.sort() 
+json_list = sorted(json_list)
+print(f"There are currently {len(json_list)} json files in the {JSON_DIR.stem} folder")
+    
+# %%
+pdf_list = []
+
+for pdf_file in DATA_DIR.glob("*.pdf"):
+    # remove pdf from end
+    #print(pdf_file.name[:-4])
+    
+    # append to list
+    pdf_list.append(pdf_file.name[:-4])
+
+pdf_list = sorted(pdf_list) 
+print(f"There are currently {len(pdf_list)} pdf files in the {DATA_DIR.stem} folder")
+
+# %%
+for json, pdf in zip(json_list, pdf_list):
+    #print(f"json: {json} pdf: {pdf}")
+    if pdf not in json_list:
+        print(f"The pdf file: {pdf} not in database")
+    
