5a7c3024573535ae39464dac586134db7c4ca5fa
lewyse lee <lewyse-lee@ons.gov.uk>
2025-03-05 22:38:37 +0000

    updated code to include url dictionary exported as a json
    script is also in-line with now updating all the pdf files

--- statschat/pdf_processing/pdf_downloader.py
+++ statschat/pdf_processing/pdf_downloader.py
@@ -1,11 +1,9 @@
-# %% [markdown]
-# Download all pdfs from certain webpage
 # %%
 import requests
+import json
 from bs4 import BeautifulSoup
 from pathlib import Path
 from urllib.parse import urlparse
-import json
 
 # %%
 # Set relative paths
@@ -16,55 +14,97 @@ if not DATA_DIR.exists():
     DATA_DIR.mkdir(parents=True, exist_ok=True)
 
 # %%
-# Extract and Parse HTML content from website
-# for ease during navigation and extraction of elements
-url = "https://www.knbs.or.ke/all-reports/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
+# get all webpages on KNBS website that have PDFs and add them to a list
+
+all_pdf_links = []
+
+page = 1
+base_url = "https://www.knbs.or.ke/all-reports/page"
+
+continue_search = True
+
+while continue_search:
+    url = base_url + str(page) + "/"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, "html.parser")
+
+    # Break the loop if no more quotes are found
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+
+    if len(pdf_links) == 0:
+        print("HAVE STOPPED GETTING WEBPAGES")
+        continue_search = False
+
+    page += 1
+    all_pdf_links.append(url)
+
+print("FINISHED COMPILING LINKS AND WILL NOW START DOWNLOADING PDFs")
 
 # %%
-# finds pdf refs on webpage
-pdf_links = [
-    a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
-]
+# print counter
 
 # %%
-# on separate lines
-webpage_list_pdf_links = "\n".join(pdf_links)
+# removes last append to list as that webpage has no pdfs (USE THIS)
+
+all_pdf_links = all_pdf_links[:-1]
+all_pdf_links
 
 # %%
-# number of pdf's
-len(pdf_links)
+# gets links for every PDF from every KNBS webpage and adds to a list
+
+all_knbs_pdf_file_links = []
+
+for pdf_file in all_pdf_links:
+    url = pdf_file
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    pdf_links = [
+        a["href"] for a in soup.find_all("a", href=True) if a["href"].endswith(".pdf")
+    ]
+
+    all_knbs_pdf_file_links.append(pdf_links)
+
+# %%
+# gets page range for PDFs to loop through multiple lists made
+pdf_page_range = len(all_knbs_pdf_file_links)
 
 # %%
 # Initalise empty dict to store url and download links
 url_dict = {}
+url_dict_filepath = f"{DATA_DIR}/url_dict.json"
+
 # %%
-# Iterate over each PDF URL extracted from the webpage,
-for pdf in pdf_links:
-    url = pdf
-    parsed_url = urlparse(url)
-    pdf_name = parsed_url.path
-    actual_pdf_file_name = pdf_name[28:]
+# downloads PDFs to relevant folder
 
-    # Download PDF and save to a local file path.
-    response = requests.get(url)
-    file_path = f"{DATA_DIR}/{actual_pdf_file_name}"
+counter = 0
 
-    # update dictionary
-    url_dict[actual_pdf_file_name] = url
-    print(url_dict[actual_pdf_file_name])
+for i in range(pdf_page_range):
+    for pdf in all_knbs_pdf_file_links[i]:
+        url = pdf
+        parsed_url = urlparse(url)
+        pdf_name = parsed_url.path
+        actual_pdf_file_name = pdf_name[28:]
 
-    # Save file in binary mode if request is successful,
-    # return error message if request fails.
-    if response.status_code == 200:
-        with open(file_path, "wb") as file:
-            file.write(response.content)
-        print(f"File {actual_pdf_file_name} downloaded successfully")
-    else:
-        print(f"Failed to download file {actual_pdf_file_name}")
+        # update dictionary
+        url_dict[actual_pdf_file_name] = url
+
+        response = requests.get(url)
+        file_path = f"{DATA_DIR}/{actual_pdf_file_name}"
+
+        if response.status_code == 200:
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            print(f"File {actual_pdf_file_name} downloaded successfully")
+
+            counter += 1
+
+        else:
+            print(f"Failed to download file {actual_pdf_file_name}")
 
 # Export url link dictionary to json file
-with open(f"{DATA_DIR}/url_dict.json", "w") as json_file:
+with open(url_dict_filepath, "w") as json_file:
     json.dump(url_dict, json_file, indent=4)
-    print("url_dict saved to url_dict.json")
+    print(f"url_dict saved to {url_dict_filepath}")
